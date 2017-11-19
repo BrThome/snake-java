7bea2bf4cbb00e0d23653619e3bfddae3682f694
Bruno Thomé <brunoaathome@gmail.com>
2017-11-19 02:37:22 -0200

    Main calls Window

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.event.KeyListener;
 
 public class Main {
 	public static void main(String[] args) {
 		Window  game = new Window(); // Gera uma nova partida
+
+		game.setTitle("Snake");
+		game.setSize(Window.height * 20, Window.width * 20);
+		game.setVisible(true);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }

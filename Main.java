c6f1f8aa404482446db96af5f54c9be4471ddb9b
Bruno Thomé <brunoaathome@gmail.com>
2017-11-19 02:50:25 -0200

    window formatting moved from Main to Window

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
-import javax.swing.JFrame;
-
 public class Main {
 	public static void main(String[] args) {
 		Window  game = new Window(); // Gera uma nova partida
 
-		game.setTitle("Snake");
-		game.setSize(Window.height * 20, Window.width * 20);
-		game.setVisible(true);
-		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
--- Window.java
+++ Window.java
@@ -1,18 +1,22 @@
 import java.awt.GridLayout;
 import javax.swing.JFrame;
+import java.awt.Color;
 
 class Window extends JFrame {
 
-	private static final long serialVersionUID = 347L; //"random"
+	private static final long serialVersionUID = 347L; // "random"
 	public static int width = 40;
 	public static int height = 40;
 
 	public Window(){
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		getContentPane().setLayout(new GridLayout(height, width, 0, 0));
 
-		
+		this.getContentPane().setBackground(Color.black);
+
+		this.setTitle("Snake");
+		this.setSize(height * 20, width * 20);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }

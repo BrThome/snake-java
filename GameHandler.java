ebcce1feffb95ab7b8b72e07373b6a71db91bf4d
Bruno Thomé <brunoaathome@gmail.com>
2017-11-19 09:25:47 -0200

    console game, godmode cheatdeath

--- GameHandler.java
+++ GameHandler.java
@@ -3,10 +3,10 @@ import java.awt.Color;
 
 class GameHandler extends Thread {
 
-	private int speed = 500; // Quanto menor, mais rapido.
+	private int speed = 200; // Quanto menor, mais rapido.
 
-	public static int width = 40;
-	public static int height = 40;
+	public static int width = 20;
+	public static int height = 20;
 	public static int dx = 1, dy = 0;
 	public static Color snakeColor = Color.green, foodColor = Color.red, backgroundColor = Color.black;
 	public static Entities ent;
@@ -30,7 +30,7 @@ class GameHandler extends Thread {
 
 			redraw();
 
-			System.out.print("HEAD x = " + ent.getHead().getX() + ", y = " + ent.getHead().getY() + "\nSIZE = " + ent.getSize() + "\nFOOD x = " + ent.getFood().getX() + ", y = " + ent.getFood().getY() + "\n");
+			//System.out.print("HEAD x = " + ent.getHead().getX() + ", y = " + ent.getHead().getY() + "\nSIZE = " + ent.getSize() + "\nFOOD x = " + ent.getFood().getX() + ", y = " + ent.getFood().getY() + "\n");
 
 			wait(speed);
 
@@ -42,9 +42,12 @@ class GameHandler extends Thread {
 	}
 
 	private boolean checkCollisionAndMove() {
+		int x = ent.getHead().getX() + ent.getHead().getDx() % GameHandler.width;
+		int y = ent.getHead().getY() + ent.getHead().getDy()  % GameHandler.height;
+
 		if(ent.isSnake(
-			ent.getHead().getX() + ent.getHead().getDx(),
-			ent.getHead().getY() + ent.getHead().getDy())) {
+			x >= 0? x : GameHandler.width - 1,
+			y >= 0? y : GameHandler.height - 1)){
 				return true;
 		}
 		if(ent.getHead().getX() +
@@ -53,6 +56,7 @@ class GameHandler extends Thread {
 			ent.getHead().getY() +
 			ent.getHead().getDy() ==
 			ent.getFood().getY()){
+				ent.setFood();
 				ent.updateSnakeArray(true);
 		}
 		else {
@@ -63,18 +67,22 @@ class GameHandler extends Thread {
 	}
 
 	private void redraw() {
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
+		for (int j = 0; j < width; j++) {
+			for (int i = 0; i < height; i++) {
 				if (ent.isSnake(i,j)) {
 					//gameWindow.add(new Pixel(snakeColor));
+					System.out.print("~");
 				}
 				else if (ent.isFood(i,j)) {
 					//gameWindow.add(new Pixel(foodColor));
+					System.out.print("@");
 				}
 				else {
 					//gameWindow.add(new Pixel(backgroundColor));
+					System.out.print(" ");
 				}
 			}
+			System.out.print("|\n");
 		}
 	}
 

a3d06a22b5ca473b6e8030c846ab3131243235f5
Bruno Alexandre Azeredo Thomé <brunoaathome@gmail.com>
2017-11-19 09:26:24 -0200

    Have no idea what i'd done.. ~ Marcio S.

--- KBListener.java
+++ KBListener.java
@@ -6,31 +6,31 @@ public class KBListener extends KeyAdapter {
 	private int playPause = 0;
 
 	public void keyPressed(KeyEvent e) {
-		switch(e.getKeyCode()) {
+		switch (e.getKeyCode()) {
 		case 39: // Muda a direcao da Snake para a direita.
 			System.out.print("Direita!");
-			if(GameHandler.dx != -1) {
+			if (GameHandler.dx != -1) {
 				GameHandler.dx = 1;
 				GameHandler.dy = 0;
 			}
 			break;
 		case 38: // Muda a direcao da Snake para cima.
 			System.out.print("Cima!");
-			if(GameHandler.dy != 1) {
+			if (GameHandler.dy != 1) {
 				GameHandler.dx = 0;
 				GameHandler.dy = -1;
 			}
 			break;
 		case 37: // Muda a direcao da Snake para a esquerda;
 			System.out.print("Esquerda!");
-			if(GameHandler.dx != 1) {
+			if (GameHandler.dx != 1) {
 				GameHandler.dx = -1;
 				GameHandler.dy = 0;
 			}
 		break;
 		case 40: // Muda a direcao da Snake para baixo.
 			System.out.print("Baixo!");
-			if(GameHandler.dy != -1) {
+			if (GameHandler.dy != -1) {
 				GameHandler.dx = 0;
 				GameHandler.dy = 1;
 			}
